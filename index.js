66129b2f2f72d5845fd65d01149c94b8e471afc4
po3rdlp <leonpolandos53@gmail.com>
2023-09-17 09:49:06 +0800

    update ssl:

--- index.js
+++ index.js
@@ -2,10 +2,17 @@ import express from "express";
 import ConnectDb from "./src/config/Dbconnect.js";
 import router from "./src/News/NewsController.js";
 import cors from "cors";
+import fs from "fs";
 
 const app = express();
 const port = 5303;
 const host = "13.228.77.82";
+
+const privateKeyPath = "/key.pem";
+const certKeyPath = "/cert.pem";
+
+const privateKey = fs.readFileSync(privateKeyPath, "utf-8");
+const certificate = fs.readFileSync(certKeyPath, "utf-8");
 
 app.use(cors());
 ConnectDb();
