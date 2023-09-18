c49cf461e0f935d2b59237de556371729f1de2f9
po3rdlp <leonpolandos53@gmail.com>
2023-09-18 17:07:19 +0800

    update:

--- index.js
+++ index.js
@@ -2,20 +2,34 @@ import express from "express";
 import ConnectDb from "./src/config/Dbconnect.js";
 import router from "./src/News/NewsController.js";
 import cors from "cors";
+import fs from "fs";
+import http from "http";
+import https from "https";
 
 const app = express();
-const port = 5303;
-const host = "13.228.77.82";
+const httpPort = 5300;
+const httpsPort = 5303;
+const host = "0.0.0.0";
+
+const keyPath = "./private-key.pem";
+const certPath = "./certificate.pem"; // Adjust the paths accordingly
 
 app.use(cors());
 ConnectDb();
 
 app.use("/", router);
 
-// app.get("/", (req, res) => {
-//   res.send("<h1>Hello</h1>");
-// });
+const options = {
+  key: fs.readFileSync(keyPath, "utf-8"),
+  cert: fs.readFileSync(certPath, "utf-8"),
+};
 
-app.listen(port, () => {
-  console.log(`Example app listening on http://${host}:${port}`);
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer(options, app);
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello</h1>");
 });
+
+httpServer.listen(httpPort, host);
+httpsServer.listen(httpsPort, host);
